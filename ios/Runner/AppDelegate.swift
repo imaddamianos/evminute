df4979551f5681862ee44893f885b811b33b7db3
iMad <damianos.imad@gmail.com>
2024-01-13 18:42:36 +0200

    fixing some map issues

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyCcolxqu3lGfKdT9DyZg2LigbmKefRlFbc")
-    GeneratedPluginRegistrant.register(with: self)
+      GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- lib/screens/home/components/chargers_map.dart
+++ lib/screens/home/components/chargers_map.dart
@@ -1,3 +1,4 @@
+// map_sample.dart
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -13,57 +14,22 @@ class MapSampleState extends State<MapSample> {
   final Completer<GoogleMapController> _controller = Completer();
 
   static const CameraPosition _initialPosition = CameraPosition(
-    target: LatLng(33.8836224, -35.5548414),
-    zoom: 10,
+    target: LatLng(37.7749, -122.4194), // San Francisco coordinates
+    zoom: 13,
   );
-
-  final Set<Marker> _markers = <Marker>{};
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Chargers Map'),
+        title: const Text('Flutter Google Maps'),
       ),
       body: GoogleMap(
         mapType: MapType.normal,
         initialCameraPosition: _initialPosition,
-        markers: _markers,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
-          // Add your customized markers
-          _addMarker(
-            const LatLng(33.8837062, 35.5583195),
-            'Marker 1',
-            'Description for Marker 1',
-            'assets/marker_icon.png', // Replace with your image asset
-          );
-          _addMarker(
-            const LatLng(33.8827062, 35.5593195),
-            'Marker 2',
-            'Description for Marker 2',
-            'assets/marker_icon.png', // Replace with your image asset
-          );
         },
-      ),
-    );
-  }
-
-  void _addMarker(
-    LatLng position,
-    String title,
-    String snippet,
-    String imagePath,
-  ) {
-    _markers.add(
-      Marker(
-        markerId: MarkerId(position.toString()),
-        position: position,
-        infoWindow: InfoWindow(
-          title: title,
-          snippet: snippet,
-        ),
-        // icon: Image.asset(name),
       ),
     );
   }
