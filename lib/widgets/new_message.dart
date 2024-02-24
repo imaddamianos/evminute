6f1ed7a220fd0abd4d37f4b7c7013478328aa78a
iMad <damianos.imad@gmail.com>
2024-02-24 18:39:32 +0200

    changing chargers icon

--- lib/widgets/new_message.dart
+++ lib/widgets/new_message.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:evminute/firebaseCalls/firebase_operations.dart';
 import 'package:evminute/models/UserModel.dart';
-// import 'package:evminute/firebaseCalls/firebase_operations.dart';
-// import 'package:evminute/models/UserModel.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
