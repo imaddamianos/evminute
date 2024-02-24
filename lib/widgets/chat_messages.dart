83774dc1e33c147a1a490cd4267bdda5d1e47b5d
iMad <damianos.imad@gmail.com>
2024-02-24 16:25:35 +0200

    implementing reading messages

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,6 +6,35 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: false)
+          .snapshots(),
+      builder: (ctx, snaphot) {
+        if (snaphot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (!snaphot.hasData || snaphot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No Message Found'),
+          );
+        }
+        if (snaphot.hasError || snaphot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('something went wrong...'),
+          );
+        }
+        final loadedMessage = snaphot.data!.docs;
+        return ListView.builder(
+          itemCount: loadedMessage.length,
+          itemBuilder: (ctx, index) => Text(
+            loadedMessage[index].data()['text'],
+          ),
+        );
+      },
+    );
   }
 }
